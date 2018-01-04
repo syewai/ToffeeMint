5c53a9098b3ae426b293eac26579229ea11f1ebc
Ma Xing <xing.ma.2015@sis.smu.edu.sg>
2018-01-04 13:16:16 +0800

    Merged with latest UI

--- app/3.FrontEnd/SMUtBank_TradeFinance/apiUpdated/SMUtBank_TradeFinance/web/assets/js/content.js
+++
@@ -1,9 +0,0 @@
-var r = new Router(
-  {
-    about: new Layout(new Page('menu.html'), new Page('about.html')),
-    home: new Layout(new Page('menu.html'), new Page('home.html')),
-    '#default': new Page('menu.html'),
-  },
-  document.querySelector('main')
-);
-
